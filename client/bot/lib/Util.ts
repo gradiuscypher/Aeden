ab1bad9335f3e71083d28c83bb51c7e456516f88
katagatame <nathan@kata.codes>
2019-05-23 14:43:01 -0400

    Removed removeInvalidMessages method

--- client/bot/lib/Util.ts
+++ client/bot/lib/Util.ts
@@ -1,5 +1,4 @@
-import { Guild } from '@yamdbf/core';
-import { Message, MessageEmbed, TextChannel } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
 
 import constants from './Constants';
 import Comic from '../lib/interfaces/nerd/Comic';
@@ -8,7 +7,6 @@ import RequestOptions from '../lib/interfaces/request/RequestOptions';
 
 import * as cheerio from 'cheerio';
 import * as req from 'request-promise';
-import MessageData from '../../../shared/lib/interfaces/MessageData';
 
 /**
  * @module Util
@@ -243,37 +241,5 @@ export default class Util {
 	 */
 	public static isNumerical(s: string | number): s is number {
 		return !Number.isNaN(Number(s.toString()));
-	}
-
-	/**
-	 * @static
-	 * @method removeInvalidMessages
-	 * @description Method to remove any messages that are invalid, or that have been deleted from a guild.
-	 * @param {string | number} s - The string to check.
-	 * @returns {boolean} Is the string numerical?
-	 */
-	public static async removeInvalidMessages(
-		editableMessages: MessageData[],
-		guild: Guild
-	): Promise<MessageData[]> {
-		const array: MessageData[] = [];
-
-		if (!editableMessages) return editableMessages;
-
-		await editableMessages.forEach(async (m): Promise<void> => {
-			if (!m.channelId) return;
-			if (!m.messageId) return;
-
-			const channel: TextChannel | undefined = (await guild.channels.get(
-				m.channelId
-			)) as TextChannel;
-			const message: Message | undefined = channel
-				? await channel.messages.get(m.messageId)
-				: undefined;
-
-			if (message) array.push(m);
-		});
-
-		return array;
 	}
 }
